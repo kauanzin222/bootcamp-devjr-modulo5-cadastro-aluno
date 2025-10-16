46a01509cc37a124efec879ca5ca954fbd6d507f
kauanzin222 <kauaanthony.cardoso44@gmail.com>
2025-10-16 15:04:33 -0300

    realizando parte da tarefa

--- js/script.js
+++ js/script.js
@@ -1,14 +1,78 @@
 
 
-var category = [
-    { id: 1, name: "Java"},
-    { id: 2, name: "Python"},
-    { id: 3, name: "C#"},
+var courses = [
+    { id: 1, name: "Java" },
+    { id: 2, name: "Python" },
+    { id: 3, name: "C#" },
 ];
 
-var turnos = [
-    { id: 1, name: "Manhã"},
-    { id: 2, name: "Tarde"},
-    { id: 3, name: "Noite"},
-];
+/*var turnos = [
+    { id: 1, name: "Manhã" },
+    { id: 2, name: "Tarde" },
+    { id: 3, name: "Noite" },
+];*/
 
+var alunos = [];
+
+function save() {
+
+    /* Colocando os valores em um vetor */
+    var aluno =
+    {
+        id: alunos.length + 1,
+        name: document.getElementById("inputName").value,
+        email: document.getElementById("inputEmail").value,
+        phone: document.getElementById("inputPhone").value,
+        course: document.getElementById("selectCourse").value,
+        turno: document.getElementsByName("gridRadios").checked,
+    }
+
+
+    addNewRow(aluno);
+
+    alunos.push(aluno);
+
+    document.getElementById("formAluno").reset();
+
+    console.log(aluno);
+};
+
+
+function addNewRow(aluno) {
+    var table = document.getElementById("tableAlunos");
+
+    var newRow = table.insertRow();
+
+    // insert id aluno
+    var idNode = document.createTextNode(aluno.id);
+    newRow.insertCell().appendChild(idNode);
+
+    // insert name aluno
+    var nameNode = document.createTextNode(aluno.name);
+    newRow.insertCell().appendChild(nameNode);
+
+    // insert email aluno
+    var emailNode = document.createTextNode(aluno.email);
+    newRow.insertCell().appendChild(emailNode);
+
+    // insert phone aluno
+    var phoneNode = document.createTextNode(aluno.phone);
+    newRow.insertCell().appendChild(phoneNode);
+
+    // insert course aluno 
+    for (let course of courses) {
+        if (course.id == aluno.course) {
+            var courseNode = document.createTextNode(course.name);
+            break;
+        }
+    }
+
+    newRow.insertCell().appendChild(courseNode);
+
+    // insert shift aluno 
+    var turno = aluno.turno.value;
+    var turnoNode = document.createTextNode(turno);
+
+    newRow.insertCell().appendChild(turnoNode);
+
+}
