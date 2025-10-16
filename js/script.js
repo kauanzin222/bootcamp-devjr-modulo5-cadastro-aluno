5ead3a5582874ab6bcf0d205022d81582ce8fa5f
kauanzin222 <kauaanthony.cardoso44@gmail.com>
2025-10-16 10:54:33 -0300

    closes #3 - adicionando estrutura da tabela e arq. js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+
+var category = [
+    { id: 1, name: "Java"},
+    { id: 2, name: "Python"},
+    { id: 3, name: "C#"},
+];
+
+var turnos = [
+    { id: 1, name: "Manhã"},
+    { id: 2, name: "Tarde"},
+    { id: 3, name: "Noite"},
+];
+
